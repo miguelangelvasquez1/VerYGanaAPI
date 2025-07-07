c838107b2eef1e27d96ba27f257e28594d9eb965
Miguel Vasquez <vgmiguel16@gmail.com>
2025-07-06 19:11:44 -0500

    Inicialización de los directorios

---
+++ src/main/java/com/Rifacel/RifacelApplication.java
@@ -0,0 +1,13 @@
+package com.Rifacel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RifacelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RifacelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Rifacel/RifacelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rifacel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RifacelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
