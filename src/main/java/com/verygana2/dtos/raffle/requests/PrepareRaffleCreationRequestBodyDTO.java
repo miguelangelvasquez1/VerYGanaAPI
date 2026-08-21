248b32ae784da49258ec276b2848846cd2a76da4
Miguel Vasquez <vgmiguel16@gmail.com>
2026-08-20 22:19:28 -0500

    merge

--- src/main/java/com/verygana2/dtos/raffle/requests/PrepareRaffleCreationRequestBodyDTO.java
+++ src/main/java/com/verygana2/dtos/raffle/requests/PrepareRaffleCreationRequestBodyDTO.java
@@ -3,7 +3,6 @@ package com.verygana2.dtos.raffle.requests;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
